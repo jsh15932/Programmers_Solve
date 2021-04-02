26043279991af20cd4aaaf66626a29ad2d7f6285
장승훈 <jsh1400512@naver.com>
2021-04-02 12:11:06 +0900

    Commit

---
+++ LEVEL2/주식가격.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        for(int i = 0; i < prices.length; i++) {
+            for(int j = i + 1; j < prices.length; j++) {
+                if(prices[i] > prices[j]) {
+                    answer[i] = j - i;
+                    
+                    break;
+                }
+                
+                if(j == answer.length - 1) {
+                    answer[i] = j - i;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
