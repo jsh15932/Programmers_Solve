ec9ff294609c0909a41662486f619463ed8b32f7
jsh15932 <jsh1400512@naver.com>
2021-04-02 10:47:44 +0900

    Create 두 개 뽑아서 더하기.java

---
+++ 두 개 뽑아서 더하기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = i + 1; j < numbers.length; j++) {
+                int tmp = numbers[i] + numbers[j];
+                
+                if(arr.indexOf(tmp) < 0) {
+                    arr.add(tmp);
+                }
+            }
+        }
+        
+        int[] answer = new int[arr.size()];
+        
+        for(int i = 0; i < arr.size(); i++) {
+            answer[i] = arr.get(i);    
+        }
+        
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
