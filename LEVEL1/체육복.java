8ac489c02984582abf8860b628e240c5051e7d5b
장승훈 <jsh1400512@naver.com>
2021-05-08 11:42:24 +0900

    Commit

--- LEVEL1/체육복.java
+++ LEVEL1/체육복.java
@@ -1,8 +1,6 @@
-import java.io.*;
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
         int[] arr = new int[n];
         
         for(int i : reserve) {
@@ -15,19 +13,17 @@ class Solution {
         
         for(int i = 0; i < arr.length; i++) {
             if(arr[i] < 0) {
-                if(i != (arr.length - 1) && arr[i + 1] > 0) {
+                if(i < (arr.length - 1) && arr[i + 1] > 0) {
                     arr[i]++;
                     arr[i + 1]--;
                 }
                 
-                else if(i != 0 && arr[i - 1] > 0) {
+                else if(i > 0 && arr[i - 1] > 0) {
                     arr[i]++;
                     arr[i - 1]--;
                 }
-            }    
+            }
         }
-        
-        int answer = 0;
         
         for(int i = 0; i < arr.length; i++) {
             if(arr[i] >= 0) {
