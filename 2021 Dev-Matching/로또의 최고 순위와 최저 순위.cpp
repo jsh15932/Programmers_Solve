31f06d02344286ce27a791266b1571522ed82284
장승훈 <jsh1400512@naver.com>
2021-05-15 14:32:46 +0900

    Commit

--- 2021 Dev-Matching/로또의 최고 순위와 최저 순위.cpp
+++ 2021 Dev-Matching/로또의 최고 순위와 최저 순위.cpp
@@ -1,33 +1,32 @@
-#include<string>
-#include<vector>
-
+#include <string>
+#include <vector>
 using namespace std;
 
 vector<int> solution(vector<int> lottos, vector<int> win_nums) {
     vector<int> answer;
-    int rank[7] = {6, 6, 5, 4, 3, 2, 1};
+    int arr[7] = {6, 6, 5, 4, 3, 2, 1};
     int min_N = 0;
     
     for(int i = 0; i < win_nums.size(); i++) {
         for(int j = 0; j < lottos.size(); j++) {
             if(lottos.at(j) == win_nums.at(i)) {
-                min_N += 1;
+                min_N++;
             }
         }
     }
     
-    int zero = 0;
+    int k = 0;
     
     for(int i = 0; i < lottos.size(); i++) {
         if(lottos.at(i) == 0) {
-            zero += 1;
+            k++;
         }
     }
     
-    int max_N = min_N + zero;
+    int max_N = min_N + k;
     
-    answer.push_back(rank[max_N]);
-    answer.push_back(rank[min_N]);
+    answer.push_back(arr[max_N]);
+    answer.push_back(arr[min_N]);
     
     return answer;
 }
