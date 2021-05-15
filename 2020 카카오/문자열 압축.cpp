26bb4e62df3619ceb1927648ad621a1f6e159301
장승훈 <jsh1400512@naver.com>
2021-05-15 17:19:54 +0900

    Commit

---
+++ 2020 카카오/문자열 압축.cpp
@@ -0,0 +1,42 @@
+#include<string>
+using namespace std;
+
+int solution(string s) {
+    int answer = s.size();
+    int n = s.size() / 2;
+
+    for (int i = 1; i <= n; i++) {
+        string res = "";
+        string tmp = s.substr(0, i);
+        int cnt = 1;
+
+        for (int j = i; j <= s.size(); j += i) {
+            if (tmp == s.substr(j, i)) {
+                cnt += 1;
+            }
+
+            else {
+                if (cnt == 1) {
+                    res += tmp;
+                }
+
+                else {
+                    res += (to_string(cnt) + tmp);
+                }
+
+                tmp = s.substr(j, i);
+                cnt = 1;
+            }
+        }
+
+        if ((s.size() % i) != 0) {
+            res += s.substr((s.size() / i) * i);
+        }
+
+        if (answer > res.size()) {
+            answer = res.size();
+        }
+    }
+
+    return answer;
+}
