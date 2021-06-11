286c8643ac4015a1ecc72ec4644095df27be2606
장승훈 <jsh1400512@naver.com>
2021-06-12 03:36:17 +0900

    Commit

---
+++ LEVEL3/가장 먼 노드.cpp
@@ -0,0 +1,49 @@
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+int solution(int n, vector<vector<int>> edge) {
+    int answer = 0;
+    int max = 0;
+    queue<int> que;
+    bool visited[20001] = {false, };
+    bool chk[20001][20001] = {false, };
+    int d[20001] = {0, };
+    
+    sort(edge.begin(), edge.end());
+    
+    for(int i = 0; i < edge.size(); i++) {
+        chk[edge[i][0]][edge[i][1]] = true;
+        chk[edge[i][1]][edge[i][0]] = true;
+    }
+    
+    que.push(1);
+    visited[1] = true;
+    
+    while(!que.empty()) {
+        int cur = que.front();
+        que.pop();
+        
+        for(int i = 2; i <= n; i++) {
+            if(chk[cur][i] == true && visited[i] == false) {
+                que.push(i);
+                visited[i] = true;
+                d[i] = d[cur] + 1;
+                
+                if(d[i] > max) {
+                    max = d[i];
+                }
+            }
+        }
+    }
+    
+    for(int i = 1; i <= n; i++) {
+        if(max == d[i]) {
+            answer++;
+        }
+    }
+    
+    return answer;
+}
