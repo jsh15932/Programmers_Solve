2eb106ff6da83a21ee83776a244f0a3ba7d927c2
장승훈 <jsh1400512@naver.com>
2021-06-11 20:11:31 +0900

    Commit

---
+++ LEVEL2/피보나치 수.cpp
@@ -0,0 +1,18 @@
+#include<string>
+#include<vector>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int arr[100001];
+    arr[0] = 0;
+    arr[1] = 1;
+    
+    for(int i = 2; i <= n; i++) {
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+    }
+    
+    answer = arr[n];
+    
+    return answer;
+}
