a223980d3f4a073791e62352e12e8da0cf61925a
장승훈 <jsh1400512@naver.com>
2021-06-23 04:06:48 +0900

    Commit

---
+++ LEVEL3/단어 변환.cpp
@@ -0,0 +1,46 @@
+#include<string>
+#include<vector>
+using namespace std;
+
+int answer = 100;
+string s;
+bool check[51];
+
+void dfs(string begin, vector<string>& words, int result) {
+    if(s == begin) {
+        answer = min(answer, result);
+        return;
+    }
+    
+    for(int i = 0; i < words.size(); i++) {
+        int count = 0;
+        for(int j = 0; j < words[i].size(); j++) {
+            if(begin[j] != words[i][j]) {
+                count++;
+            }
+            
+            if(count == 2) {
+                break;
+            }
+        }
+        
+        if(count == 1) {
+            if(check[i] == false) {
+                check[i] = true;
+                dfs(words[i], words, result + 1);
+                check[i] = false;
+            }
+        }    
+    }
+}
+
+int solution(string begin, string target, vector<string> words) {
+    s = target;
+    dfs(begin, words, 0);
+    
+    if(answer == 100) {
+        answer = 0;
+    }
+    
+    return answer;
+}
