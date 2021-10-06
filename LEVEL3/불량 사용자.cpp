f9cc1d0f454ba839e8a9f9fbf67f6a133282e8de
jsh15932 <jsh1400512@naver.com>
2021-10-06 14:21:04 +0900

    Commit

--- LEVEL3/불량 사용자.cpp
+++ LEVEL3/불량 사용자.cpp
@@ -1,65 +1,67 @@
-#include<string>
-#include<set>
-#include<vector>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
-int user_len, banned_len;
-bool chk[8];
+vector<string> user;
+vector<string> banned;
+bool visited[10];
 set<string> s;
 
-void solve(int idx, string res, vector<string> user_id, vector<string> banned_id) {
-    if(idx == banned_len) {
-        sort(res.begin(), res.end());
-        s.insert(res);
+void solve(int idx) {
+    if(idx == banned.size()) {
+        string tmp = "";
+        
+        for(int i = 0; i < user.size(); i++) {
+            if(visited[i]) {
+                tmp += user[i];
+            }
+        }
+        
+        s.insert(tmp);
         
         return;
     }
     
-    string b_id = banned_id[idx];
-    int b_len = b_id.size();
-    
-    for(int i = 0; i < user_len; i++) {
-        string u_id = user_id[i];
+    for(int i = 0; i < user.size(); i++) {
+        bool chk = true;
         
-        if(u_id.size() != b_len) {
+        if(visited[i]) {
             continue;
         }
         
-        if(chk[i]) {
+        if(user[i].length() != banned[idx].length()) {
             continue;
         }
         
-        bool flag = true;
-        
-        for(int j = 0; j < b_len; j++) {
-            if(b_id[j] == '*') {
+        for(int j = 0; j < user[i].length(); j++) {
+            if(banned[idx][j] == '*') {
                 continue;
             }
             
-            if(u_id[j] != b_id[j]) {
-                flag = false;
+            if(banned[idx][j] != user[i][j]) {
+                chk = false;
                 
                 break;
             }
         }
         
-        if(flag) {
-            chk[i] = true;
-            solve(idx + 1, res + to_string(i), user_id, banned_id);
-            
-            chk[i] = false;
+        if(chk) {
+            visited[i] = true;
+            solve(idx + 1);
+            visited[i] = false;
         }
     }
+    
+    return;
 }
 
 int solution(vector<string> user_id, vector<string> banned_id) {
     int answer = 0;
     
-    user_len = user_id.size();
-    banned_len = banned_id.size();
+    user = user_id;
+    banned = banned_id;
     
-    solve(0, "", user_id, banned_id);
+    solve(0);
+    
     answer = s.size();
     
     return answer;
