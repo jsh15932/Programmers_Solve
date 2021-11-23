d6c3aeb487d1cbfbae490aab5a7fd8180369158a
jsh15932 <jsh1400512@naver.com>
2021-11-23 20:29:20 +0900

    Commit

--- LEVEL2/구명보트.cpp
+++ LEVEL2/구명보트.cpp
@@ -1,20 +1,21 @@
-#include<string>
-#include<vector>
-#include<algorithm>
+#include <string>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int solution(vector<int> people, int limit) {
     int answer = 0;
-    sort(people.begin(), people.end());
     int idx = 0;
+    sort(people.begin(), people.end());
     
     while(idx < people.size()) {
-        int p = people.back();
+        int back = people.back();
         people.pop_back();
         
-        if(people[idx] + p <= limit) {
-            idx++;
+        if(people[idx] + back <= limit) {
             answer++;
+            idx++;
         }
         
         else {
