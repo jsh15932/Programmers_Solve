19228641ae1c6f167593aa0704783838928ce703
jsh15932 <jsh1400512@naver.com>
2021-11-23 19:06:25 +0900

    Commit

--- LEVEL1/체육복.cpp
+++ LEVEL1/체육복.cpp
@@ -1,29 +1,30 @@
-#include<string>
-#include<vector>
+#include <string>
+#include <vector>
+
 using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
     int answer = 0;
     vector<int> arr(n);
     
-    for(int i : reserve) {
-        arr[i - 1]++;
-    }
-    
     for(int i : lost) {
         arr[i - 1]--;
     }
     
+    for(int i : reserve) {
+        arr[i - 1]++;
+    }
+    
     for(int i = 0; i < arr.size(); i++) {
         if(arr[i] < 0) {
-            if(i < (arr.size() - 1) && arr[i + 1] > 0) {
+            if(i > 0 && arr[i - 1] > 0) {
+                arr[i]++;
+                arr[i - 1]--;
+            }
+        
+            else if(i < (arr.size() - 1) && arr[i + 1] > 0) {
                 arr[i]++;
                 arr[i + 1]--;
-            }
-            
-            else if(i > 0 && arr[i - 1] > 0) {
-                arr[i]++;
-                arr[i - 1]++;
             }
         }
     }
@@ -33,6 +34,6 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
             answer++;
         }
     }
-
+    
     return answer;
 }
